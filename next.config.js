081ca4503f54de74210f9306b513b692a1036c78
PavelTrofff <charmanderchik1111@gmail.com>
2025-02-13 03:33:25 +0000

    Fix Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  distDir: 'dist'
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
